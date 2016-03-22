d3453eea8e0b7a60f213e070838824b39ac4de06
fieldOfView <aldo@fieldofview.com>
2016-03-22 10:47:01 +0100

    Add a basic material listing page
    IMPORTANT: MaterialsModel.py and the material profiles in resources/materials are strictly mockups to provide data for the materials page. At this point the materials on the materialspage are not used in Cura!
    
    CURA-339

--- cura/CuraApplication.py
+++ cura/CuraApplication.py
@@ -34,6 +34,7 @@ from . import CuraActions
 from . import MultiMaterialDecorator
 from . import ZOffsetDecorator
 from . import CuraSplashScreen
+from . import CuraBindings
 
 from PyQt5.QtCore import pyqtSlot, QUrl, pyqtSignal, pyqtProperty, QEvent, Q_ENUMS
 from PyQt5.QtGui import QColor, QIcon
@@ -63,6 +64,7 @@ class CuraApplication(QtApplication):
     class ResourceTypes:
         QmlFiles = Resources.UserType + 1
         Firmware = Resources.UserType + 2
+        Materials = Resources.UserType + 3
     Q_ENUMS(ResourceTypes)
 
     def __init__(self):
@@ -107,6 +109,7 @@ class CuraApplication(QtApplication):
 
         Resources.addType(self.ResourceTypes.QmlFiles, "qml")
         Resources.addType(self.ResourceTypes.Firmware, "firmware")
+        Resources.addType(self.ResourceTypes.Materials, "materials")
 
         Preferences.getInstance().addPreference("cura/active_machine", "")
         Preferences.getInstance().addPreference("cura/active_mode", "simple")
@@ -197,6 +200,7 @@ class CuraApplication(QtApplication):
 
         self.setMainQml(Resources.getPath(self.ResourceTypes.QmlFiles, "Cura.qml"))
         self.initializeEngine()
+        CuraBindings.CuraBindings.register()
 
         if self._engine.rootObjects:
             self.closeSplash()
